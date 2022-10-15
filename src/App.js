1c39d646a38c16807d625f6531a7717ee4584a45
Harley <yu.h.lin1991@gmail.com>
2022-10-16 00:39:23 +0800

    set up all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Homepage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../components/Search";
+
+function Homepage() {
+  const auth = "563492ad6f9170000100000125c21e996ae6487abb6d8e349a3f2684";
+  return (
+    <div>
+      <h1>My Homepage</h1>
+    </div>
+  );
+}
+
+export default Homepage;
