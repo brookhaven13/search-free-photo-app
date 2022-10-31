e25da0736f53a358c5880c6701abe3b761eb1de4
Harley <yu.h.lin1991@gmail.com>
2022-10-31 22:54:50 +0800

    implemnet mui-masonry

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,6 +3,7 @@ import "./../styles/style.css";
 import Search from "../components/Search";
 import Photo from "../components/Masonry";
 import { getCuratedPhotos, SearchPhotos, loadMorePhotos } from "./../pexels";
+import { Masonry } from "@mui/lab";
 
 function Homepage() {
   const [data, setData] = useState(null);
@@ -36,10 +37,12 @@ function Homepage() {
     <div className="homepage-section">
       <Search search={search} />
       <div className="masonry-blk">
-        {data &&
-          data.map((d) => {
-            return <Photo data={d} />;
-          })}
+        <Masonry columns={4} spacing={2}>
+          {data &&
+            data.map((d) => {
+              return <Photo data={d} />;
+            })}
+        </Masonry>
       </div>
       <div className="buttom">
         <button onClick={loadNextPage}>Load More</button>
